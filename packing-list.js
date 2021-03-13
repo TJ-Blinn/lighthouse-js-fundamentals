bc676b349cce0f5305f95bf844cf7db3184e8356
TJ-Blinn <tjblinn@hotmail.com>
2021-03-13 02:34:26 +0000

    Iterating over Arrays for & while loops

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,20 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+
+let i = 0;
+while (i < packingListList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+/*
+console.log("Kitchen stuff to pack:");
+
+for (let i = 0; i < packingList.length; i++) {  // for loop
   console.log(packingList[i]);
 }
-// we're declaring a counter variable called i (short for "index") that starts at 0, and increments by 1 
+// we're declaring a counter variable called i (short for "index") that starts at 0, and increments by 1
 //at every step of a loop until it matches the length of our array, that is packingList.
+*/
+
