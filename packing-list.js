90fbd4a58890788fcca1b5a0fc58001dec3c41ad
TJ-Blinn <tjblinn@hotmail.com>
2021-03-13 02:27:47 +0000

    Iterating over Arrays

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+// we're declaring a counter variable called i (short for "index") that starts at 0, and increments by 1 
+//at every step of a loop until it matches the length of our array, that is packingList.
